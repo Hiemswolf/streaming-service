ac5467286258245e83e045d67bd46a2b6275bbd1
Hiemswolf <claytonkruse08@gmail.com>
2022-01-11 23:24:38 -0600

    nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -17,20 +17,22 @@
 
 <a id="skip-nav" href="#nav-end">Skip Navagation</a>
 <nav id="nav" class="nav">
-	<div>
-		<a sveltekit:prefetch href="/" class="home-link" on:click|preventDefault={() => goto('/')}
-			><h1>Service</h1></a
-		>
-		<Search />
+	<div class="nav__logo">
+		<a sveltekit:prefetch href="/" on:click|preventDefault={() => goto('/')}><h1>Service</h1></a>
 	</div>
-	<div class="login-buttons">
-		{#if loggedIn}
-			<strong>{$user.nick || $user.name}</strong>
-			<button on:click={logOut}>Log Out</button>
-		{:else}
-			<button on:click={logIn}>Log In</button>
-			<a href="/sign-up"><button>Sign Up</button></a>
-		{/if}
+
+	<div class="nav__body">
+		<Search />
+
+		<div class="nav__login-buttons">
+			{#if loggedIn}
+				<strong>{$user.nick || $user.name}</strong>
+				<button on:click={logOut}>Log Out</button>
+			{:else}
+				<button on:click={logIn}>Log In</button>
+				<a href="/sign-up"><button>Sign Up</button></a>
+			{/if}
+		</div>
 	</div>
 </nav>
 <div id="nav-end" />
